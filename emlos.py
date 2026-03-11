3a08f03c712cfa5c9bfad5a71b17adedb54c8b2c
Varga Attila <varga.attila@diak.szbi-pg.hu>
2026-03-11 08:42:24 +0100

    Macska és kutya al-osztályok

---
+++ emlos.py
@@ -0,0 +1,22 @@
+from allat import Allat
+class Emlos(Allat):
+    def __init__(self, nev_, faj_, eletkor_, elohely_, szorzet_szine_):
+        super().__init__(nev_, faj_, eletkor_, elohely_, "közepes")
+        self.szorzet_szine = szorzet_szine_
+    
+    def __str__(self):
+        return f"{self.nev}, faj: {self.faj}, életkor: {self.eletkor}, élőhelye: {self.elohely}, méret: {self.meret}, szőrzete: {self.szorzet_szine}"
+
+class Macska(Emlos):
+    def __init__(self, nev_, eletkor_, elohely_,  szorzet_szine_):
+        super().__init__(nev_, "macska", eletkor_, elohely_, szorzet_szine_)
+    
+    def dorombol(self):
+        print(f"{self.nev} dorombol.")
+
+class Kutya(Emlos):
+    def __init__(self, nev_, eletkor_, elohely_, szorzet_szine_):
+        super().__init__(nev_, "kutya", eletkor_, elohely_, szorzet_szine_)
+    
+    def ugat(self):
+        print(f"{self.nev} ugat")
--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 from allat import Allat
+from emlos import Emlos, Macska, Kutya
 
 allat1 = Allat("Bodri", "kutya", 5, "kert", "közepes")
 allat2 = Allat("Cirmi", "macska", 3, "ház", "közepes")
 
 print(f"Allat 1: {allat1}")
 print(f"Allat 2: {allat2}")
+
+print("\nEmlősök:\n")
+
+emlos1 = Emlos("Morzsi", "kutya", 5, "kert", "barna")
+emlos2 = Emlos("Cirmi", "Macska", 3, "Hát", "cirmos")
+
+print(emlos1)
+print(emlos2)
+
+print("\nMacska:\n")
+
+macska1 = Macska("Cirmi", 4,"ház", "sziámi")
+
+print(macska1)
+macska1.dorombol()
+
+print("\nKuya:\n")
+
+kutya1 = Kutya("Morzsa", 5, "kert", "barna")
+print(kutya1)
+kutya1.ugat()
